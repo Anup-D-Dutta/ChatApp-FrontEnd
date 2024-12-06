632cd4763d76071b6627a0d2f26712f6c49d2034
Anup-D-Dutta <codewithanup@gmail.com>
2024-12-06 14:26:18 +0530

    fixed frontend

--- src/pages/admin/UserManagement.jsx
+++ src/pages/admin/UserManagement.jsx
@@ -3,7 +3,7 @@ import AdminLayout from '../../components/layout/AdminLayout'
 import Table from '../../components/Shared/Table'
 import { Avatar } from '@mui/material'
 import { dashboardData } from '../../constants/sampleData'
-import {transformImage} from '../../lib/features'
+import {transformImage} from '../../lib/Features'
 
 
 // (Not Work) Error = Data is not being received from sampledata
